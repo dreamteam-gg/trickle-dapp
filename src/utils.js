1818bc7ea16a61af89069e2d8f0ba1b087c09aa0
Nikita Savchenko <zitros.lab@gmail.com>
2019-04-20 23:51:28 +0300

    Refinements of agreement terms page

--- src/utils.js
+++ src/utils.js
@@ -13,3 +13,7 @@ export function shortenEthereumAddress (address) {
     }
     return address.substr(0, 6) + ".." + address.substr(-4);
 }
+
+export function getEtherscanLinkToAddress (address, networkName) {
+    return `https://${ networkName === "homestead" || !networkName ? "" : (networkName + ".") }etherscan.io/address/${ address }`;
+}
