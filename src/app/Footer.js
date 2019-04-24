df09e059df5c1d6d148a0752cb1abb5d5732c475
Nikita Savchenko <zitros.lab@gmail.com>
2019-04-24 15:19:11 +0300

    Update & build

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -6,7 +6,7 @@ export default class NavBar extends Component {
     render () {
         return <div className="footer">
             <div>Decentralized monetary smart agreements service is provided "as is"</div>
-            <div>Powered by DreamTeam</div>
+            <div>Powered by <a target="_blank" href="https://dreamteam.gg">DreamTeam</a></div>
         </div>
     }
 
